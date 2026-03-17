b8964c03a6c4dba82c418951d087cd4a79d59fc5
CashNoob <foxtrotdiscord69@gmail.com>
2026-03-17 13:54:59 -0500

    big

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let beanAmount = 0;
+/*--Variables--*/
+
+const bean = document.querySelector(".bean");
+let beanAmount = parseInt(localStorage.getItem('beans')) || 0;
 let beanDisplay = document.getElementById("beans");
 let beanPic = document.getElementById("beanpic");
 
@@ -7,13 +10,53 @@ let beanPic = document.getElementById("beanpic");
 function beanclicker() {
     beanAmount++;
     beanDisplay.textContent = beanAmount;
+    localStorage.setItem('beans', beanAmount);
+    spawnFloatText();
+}
+
+function spawnFloatText() {
+    const text = document.createElement("div");
+    text.textContent = "+1";
+    text.className = "float-text";
+
+    document.body.appendChild(text);
+
+    const rect = beanPic.getBoundingClientRect();
+    text.style.left = (rect.left + rect.width / 2 + (Math.random() * 80 - 40)) + "px";
+    text.style.top = (rect.top + (Math.random() * 60 - 30)) + "px";
+
+    setTimeout(() => text.remove(), 800);
+}
+
+function initialize() {
+    beanDisplay.textContent = beanAmount;
+}
+
+function devreset() {
+    localStorage.setItem("beans", 0);
+    beanAmount = localStorage.getItem("beans");
+    beanDisplay.textContent = beanAmount;
 }
 
 
-
-
-
-
-/*--Listeners--*/
+/*--Events--*/
 
 beanPic.addEventListener("click", beanclicker)
+
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+});
+
+bean.addEventListener('click', () => {
+    bean.classList.remove('bean-clicked');
+    void bean.offsetWidth;
+    bean.classList.add('bean-clicked');
+});
+
+bean.addEventListener('animationend', () => {
+  bean.classList.remove('bean-clicked');
+});
+
+/*--Start--*/
+
+initialize();
